cee1288f35d0b84f62f035d636fe2a144fc76670
kysley <evan@kysley.com>
2022-01-02 16:50:48 -0500

    build debugging

--- src/hooks/use-weave-code.hook.ts
+++ src/hooks/use-weave-code.hook.ts
@@ -1,10 +1,9 @@
 import ky from "ky";
 import { useMutation, useQuery } from "react-query";
-console.log(process.env.CONTEXT);
-const url =
-  process.env.CONTEXT === "production"
-    ? "https://api.e8y.fun/weave"
-    : "http://localhost:3001/";
+
+const url = process.env.prod
+  ? "https://api.e8y.fun/weave"
+  : "http://localhost:3001/";
 
 export function useWeaveCode(code: string) {
   return useQuery(
